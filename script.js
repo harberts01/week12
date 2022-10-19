972cb2ea475230cfb9913a6ab2b236f21ce46289
Phil Harberts <109708014+harberts01@users.noreply.github.com>
2022-10-18 21:39:40 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,124 @@
 class Cookbook {
-    constructor(name){
+    constructor(name) {
         this.name = name;
-        this.recipes =[];
+        this.recipes = [];
+    }
+
+    addRecipe(name) {
+        this.recipes.push(new Recipe(name));
     }
 }
 
-class Recipes {
-    constructor (name, ingredients){
+class Recipe {
+    constructor(name){
         this.name = name;
-        this.ingredients = ingredients;
+        this.ingredients = [];
+    }
+}
+
+class Ingredients {
+    constructor(name, amount, measurement){
+        this.name = name;
+        this.amount = amount;
+        this.measurement = measurement;
     }
 }
 
 class CookbookService {
-    static url = "https://"
+    static url = 'https://634eed79df22c2af7b46c545.mockapi.io/cookbook'
 
-    static getAllRecipes(){
+    static getAllCookbooks(){
         return $.get(this.url);
     }
-    static getRecipe(id) {
-        return $.get(this.url + `/${id}`);
+
+    static getCookbook(id) {
+        return $.get(this.url + `/${id}`)
+    }
+
+    static createCookbook(cookbook) {
+        return $.post(this.url, cookbook);
+    }
+
+    static updateCookbook(cookbook){
+        return $.ajax({
+            url: this.url + `/${cookbook._id}`,
+            dataType: 'json',
+            data: JSON.stringify(cookbook),
+            contentType: 'application/json',
+            type: 'PUT'
+        });
+    }
+
+    static deleteCookbook(id) {
+        return $.ajax({
+            url: this.url+`/${id}`,
+            type: 'DELETE'
+        });
     }
 }
-//not sure if this is needed for my app
+
+class DOMManager {
+    static cookbooks;
+
+    static getAllCookbooks() {
+        CookbookService.getAllCookbooks().then(cookbooks =>this.render(cookbooks));
+    }
+
+    static deleteCookbook(id) {
+       CookbookService.deleteCookbook(id)
+        .then(() =>{
+            return CookbookService.getAllCookbooks();
+        })
+        .then((cookbooks) => this.render(cookbooks));
+    }
+
+    static createCookbook(name) {
+        CookbookService.createCookbook(new Cookbook(name))
+        .then(() => {
+            return CookbookService.getAllCookbooks();
+        })
+        .then((cookbooks) => this.render(cookbooks));
+    }
+
+    static render(cookbooks) {
+        this.cookbooks = cookbooks;
+        $('#app').empty();
+        for (let cookbook of cookbooks) {
+            $('#app').prepend(`
+                           <div id="${cookbook._id}" class="card text-center mb-5">
+                                <div class="card-header">
+                                    <h2>${cookbook.name}</h2>
+                                    <button class="btn btn-success" onclick="DOMManager.updateCookbook('${cookbook._id}')">Open Your ${cookbook.name} Cookbook</button>
+                                    <button class="btn btn-danger" onclick="DOMManager.deleteCookbook('${cookbook._id}')">Delete</button>
+                                </div>
+                            </div>`
+            );
+            for (let recipe of cookbook.recipes) {
+
+            }
+        }
+    }
+}
+
+$('#new-cookbook-btn').click(() => {
+    DOMManager.createCookbook($('#new-cookbook-name').val());
+    $('new-cookbook-name').val('');
+})
+
+
+DOMManager.getAllCookbooks();
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
