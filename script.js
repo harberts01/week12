0d3ed8652dff67a1a47963a78ae659def8d801ed
harberts01 <109708014+harberts01@users.noreply.github.com>
2022-10-22 12:52:05 -0500

    committing project files

--- script.js
+++ script.js
@@ -2,33 +2,18 @@ class Cookbook {
     constructor(name) {
         this.name = name;
         this.recipes = [];
-        
     }
 
     addRecipe(name) {
         this.recipes.push(new Recipe(name));
     }
-
-    
 }
 class Recipe {
     constructor(name){
         this.name = name;
-        this.ingredients = [];
-    }
-
-    addIngredients(name, amount, measurement) {
-        this.ingredients.push(new Ingredients(name, amount, measurement));
     }
 }
 
-class Ingredients {
-    constructor(name, amount, measurement){
-        this.name = name;
-        this.amount = amount;
-        this.measurement = measurement;
-    }
-}
 
 class CookbookService {
     static url = 'https://634eed79df22c2af7b46c545.mockapi.io/cookbook'
@@ -100,12 +85,12 @@ class DOMManager {
         .then((cookbooks) => this.renderCookbooks(cookbooks));
     }
 
-    static deleteRecipe(cookbookId, recipeId){
+    static deleteRecipe(cookbookId, recipeName){
         for (let cookbook of this.cookbooks){
             if (cookbook.id == cookbookId){
                 for(let recipe of cookbook.recipes){
-                    if(recipe.id == recipeId){
-                        cookbook.recipe.splice(cookbook.recipe.indexOf(recipe), 1);
+                    if(recipe.name == recipeName){
+                        cookbook.recipes.splice(cookbook.recipes.indexOf(recipe), 1);
                         CookbookService.updateCookbook(cookbook)
                             .then(() => {
                                 return CookbookService.getAllCookbooks();
@@ -128,7 +113,7 @@ class DOMManager {
                 <div id="${cookbook.id}" class="card text-center mb-5">
                     <div class="card-header">
                         <h2>${cookbook.name}</h2>
-                        <button class="btn btn-success" onclick="DOMManager.renderRecipes('${cookbook.id}')">Open Your ${cookbook.name} Cookbook</button>
+                        <!-- <button class="btn btn-success" onclick="DOMManager.renderRecipes('${cookbook.id}')">Open Your ${cookbook.name} Cookbook</button> -->
                         <button class="btn btn-danger" onclick="DOMManager.deleteCookbook('${cookbook.id}')">Delete</button>
                     </div>
                     <div class="card-body">
@@ -141,7 +126,7 @@ class DOMManager {
                                     <input type="text" id="${cookbook.id}-recipe-name" class="form-control" placeholder="Recipe Name">
                                 </div> -->
                             </div>
-                            <button id="${cookbook.id}-new-recipe" onclick="DOMManager.addRecipe('${cookbook.id}')" class="btn btn-success formcontrol">Add</button>
+                            <button id="${cookbook.id}-new-recipe" onclick="DOMManager.addRecipe('${cookbook.id}')" class="btn btn-success formcontrol mb-4">Add</button>
                         </div>
                     </div>
                 </div><br>
@@ -149,8 +134,10 @@ class DOMManager {
             for(let recipe of cookbook.recipes) {
                 $(`#${cookbook.id}`).find('.card-body').append(`
                 <p>
-                    <span id="name-${recipe.id}"><strong>Recipe: </strong>${recipe.name}</span>
-                    <button class="btn btn-danger" onclick="DOMManager.deleteRecipe('${cookbook.id}', '${recipe.id}')">Delete Recipe</button>
+                    <span id="name-${recipe.name}"><strong>Recipe: </strong>${recipe.name}</span>
+                    <button class="btn btn-success" onclick="#">Open</button>
+                    <button class="btn btn-danger" onclick="DOMManager.deleteRecipe('${cookbook.id}', '${recipe.name}')">Delete Recipe</button>
+                    
                 </p>
                 
                 `)
